8bf8b9353d76cdff33770a6f694cac80c0911ae6
Jeong Hi <tresorhi@naver.com>
2023-02-22 20:36:53 +0900

    페이지 목록 출력

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -1,14 +1,18 @@
 package org.zerock.b01.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.b01.dto.BoardDTO;
 import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.service.BoardService;
 
 @Controller
+@Log4j2
 @RequestMapping("/board")
 @RequiredArgsConstructor
 public class BoardController {
@@ -16,7 +20,11 @@ public class BoardController {
 
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model) {
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
 
+        log.info(responseDTO);
+
+        model.addAttribute("responseDTO", responseDTO);
     }
 
 }
