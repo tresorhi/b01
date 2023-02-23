8fac54299acf599485a9b6fc0229c42f62979f30
Jeong Hi <tresorhi@naver.com>
2023-02-24 04:40:29 +0900

    댓글 등록

--- src/main/java/org/zerock/b01/controller/ReplyController.java
+++ src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -4,12 +4,16 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.b01.dto.ReplyDTO;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -18,11 +22,19 @@ import java.util.Map;
 public class ReplyController {
     @ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글 등록")
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, Long>> register(@RequestBody ReplyDTO replyDTO) {
+    public Map<String, Long> register(
+            @Valid @RequestBody ReplyDTO replyDTO,
+            BindingResult bindingResult) throws BindException {
+
         log.info(replyDTO);
 
-        Map<String, Long> resultMap = Map.of("rno", 111L);
+        if (bindingResult.hasErrors()) {
+            throw new BindException(bindingResult);
+        }
 
-        return ResponseEntity.ok(resultMap);
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno", 111L);
+
+        return resultMap;
     }
 }
--- src/main/java/org/zerock/b01/dto/ReplyDTO.java
+++ src/main/java/org/zerock/b01/dto/ReplyDTO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -15,10 +17,13 @@ public class ReplyDTO {
 
     private Long rno;
 
+    @NotNull
     private Long bno;
 
+    @NotEmpty
     private String replyText;
 
+    @NotEmpty
     private String replyer;
 
     private LocalDateTime regDate, modDate;
